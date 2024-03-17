492087b1bccd61bc2a1731e40c89158e48d56552
Leonardo Caldeira <leob.caldeira@gmail.com>
2024-03-17 15:34:18 -0300

    fazendo fetch dos dados do db e colocando as imagens no public

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,34 +1,48 @@
-import Header from 'components/Header';
-import styles from './Home.module.scss';
-import relogio from 'assets/inicial.png';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Button from 'components/Button';
+import Header from "components/Header";
+import styles from "./Home.module.scss";
+import relogio from "assets/inicial.png";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Button from "components/Button";
+import { useCallback, useEffect } from "react";
+import instance from "common/config/api";
+import { adicionarCategorias } from "store/reducers/categorias";
 
 export default function Home() {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-  const categorias = useSelector(state => state.categorias);
+  const categorias = useSelector((state) => state.categorias);
+
+  const buscarCategorias = useCallback(async () => {
+    const resposta = await instance.get("/categorias");
+
+    dispatch(adicionarCategorias(resposta.data));
+  }, [dispatch]);
+
+  useEffect(() => {
+    buscarCategorias();
+  }, [buscarCategorias]);
+
   return (
     <div>
       <Header
-        titulo='Classificados Tech'
-        descricao='Compre diversos tipos de produtos no melhor site do Brasil!'
+        titulo="Classificados Tech"
+        descricao="Compre diversos tipos de produtos no melhor site do Brasil!"
         imagem={relogio}
         className={styles.header}
       >
-        <Button onClick={() => navigate('/anuncie')}>
-          Quero anunciar
-        </Button>
+        <Button onClick={() => navigate("/anuncie")}>Quero anunciar</Button>
       </Header>
       <div className={styles.categorias}>
-        <div className={styles['categorias-title']}>
-          <h1>
-            Categorias
-          </h1>
+        <div className={styles["categorias-title"]}>
+          <h1>Categorias</h1>
         </div>
-        <div className={styles['categorias-container']}>
+        <div className={styles["categorias-container"]}>
           {categorias.map((categoria, index) => (
-            <div key={index} onClick={() => navigate(`/categoria/${categoria.id}`)}>
+            <div
+              key={index}
+              onClick={() => navigate(`/categoria/${categoria.id}`)}
+            >
               <img src={categoria.thumbnail} alt={categoria.nome} />
               <h1>{categoria.nome}</h1>
             </div>
@@ -36,5 +50,5 @@ export default function Home() {
         </div>
       </div>
     </div>
-  )
+  );
 }
